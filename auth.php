d3095538c9c1ab3e15a791b6c25a1d19081f55ac
Vyacheslav Lynnyk <lynnyk@redoo-networks.com>
2016-12-03 21:45:25 +0200

    Auth styles updated

--- auth.php
+++ auth.php
@@ -16,17 +16,16 @@ if (isset($_POST['pass']) && sha1($_POST['pass']) == '1f1335fb0a6f530f9104532f65
 <head>
 	<meta charset="UTF-8">
 	<title>Auth</title>
-	<link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" href="./styles/main.css">
 </head>
 <body>
 
-	<div style="width: 320px; margin: calc(50vh - 80px) auto; text-align: center">
-		<form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
-
-			<h1 style="color: #999;font-family: Arial,sans-serif;">Enter the password:</h1>
-	
+	<div style="width: 380px; margin: calc(50vh - 210px) auto; text-align: center">
+		<form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST" style="text-align: center">
+			<img src="imgs/weather-logo.png" alt="weather">
+			<img src="imgs/enter.png" alt="enter">
 			<input type="password" name='pass' required="required" autofocus>
-			<button type="submit">Enter</button>					
+			<button type="submit">Войти</button>
 		</form>
 	</div>
 
